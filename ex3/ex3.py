165e43edc72719b9c353553631c80162caf336fd
Syed Adeebul Hafiz <29689720+dadhaf@users.noreply.github.com>
2018-01-20 23:11:52 +0530

    ex3

---
+++ ex3/ex3.py
@@ -0,0 +1,21 @@
+print 'I will now count my chickens:'
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4 #Ampersand has lower priority than multiplication
+
+print 'Now I will count the eggs:'
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #
+
+print "Is it true that 3+2<5-7?"
+
+print 3 + 2 < 5 - 7  #bool statement
+
+print "What is 3+2?", 3 + 2, #NOTICE how this will affect the output
+print "What is 5-7?", 5 - 7
+
+print "Oh, thats why it's False."
+
+print "Is it greater?",5 > -2
+print "Is it greater or equal?",5 >= -2
+print "Is it less or equl>", 5 <= -2
